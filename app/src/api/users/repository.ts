b72225f4f19b1a926888ea69c8da3bc82d25190c
eyemovic-doi <doi@eyemovic.com>
2024-02-08 17:42:46 +0900

    chore: add comments

--- app/src/api/users/repository.ts
+++ app/src/api/users/repository.ts
@@ -3,13 +3,28 @@ import { db } from "../../drizzle/db";
 import { $users } from "../../drizzle/schema";
 import { User } from "../../drizzle/schema";
 
+/**
+ * ユーザーをIDで検索する
+ * @param id
+ * @returns
+ */
 export const findUserById = async (id: number): Promise<User | undefined> => {
 	return await db.select().from($users).where(eq($users.id, id)).limit(1).get();
 };
+
+/**
+ * ユーザーを全件取得する
+ * @returns
+ */
 export const findUsers = async (): Promise<Array<User>> => {
 	return await db.select().from($users);
 };
 
+/**
+ * ユーザーを作成する
+ * @param user
+ * @returns
+ */
 export const createUser = async (user: User): Promise<void> => {
 	return await db.insert($users).values(user);
 };
