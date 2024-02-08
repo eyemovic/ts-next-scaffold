68698fa91b186fcbd5d79fec20dfd2e06bc89ffc
eyemovic-doi <doi@eyemovic.com>
2024-02-09 00:02:10 +0900

    fix: schema

--- app/src/api/users/schema/schema.ts
+++ app/src/api/users/schema/schema.ts
@@ -2,7 +2,7 @@ import { Type as T } from "@sinclair/typebox";
 
 export const UserSchema = T.Object({
 	id: T.Number(),
-	firstName: T.Union([T.String(), T.Null()]),
-	givenName: T.Union([T.String(), T.Null()]),
-	createdAt: T.Union([T.String(), T.Null()]),
+	firstName: T.String(),
+	givenName: T.String(),
+	createdAt: T.String(),
 });
