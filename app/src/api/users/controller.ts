bdc5f02150526a36ada298bafd8030fe1ed724e2
eyemovic-doi <doi@eyemovic.com>
2024-02-09 00:27:24 +0900

    refactor: compose functions

--- app/src/api/users/controller.ts
+++ app/src/api/users/controller.ts
@@ -1,29 +1,46 @@
 import { tbValidator } from "@hono/typebox-validator";
+import { Type as T } from "@sinclair/typebox";
+import { Context } from "hono";
 import { HTTPException } from "hono/http-exception";
 import app from "..";
 import { User } from "../../db/types";
 import { GLOBAL_MESSAGE, USER_MESSAGE } from "../constant";
-import { createUser, findUserById, findUsers } from "./repository";
+import { UserRepository } from "./repository";
 import { UserSchema } from "./schema/schema";
 
 /**
- * User
+ * UserController
+ * @description ユーザー情報に関するコントローラー
+ */
+const UserController = {
+	getUserById: (c: Context) => {
+		const id = c.req.param("id");
+		return c.json(UserRepository.findUserById(parseInt(id)));
+	},
+	getUsers: (c: Context) => {
+		return c.json(UserRepository.findUsers());
+	},
+	postUser: tbValidator("json", UserSchema, (result, c) => {
+		if (!result.success) {
+			throw new HTTPException(400, { message: GLOBAL_MESSAGE.INVALID_REQUEST });
+		}
+		UserRepository.createUser(result.data satisfies User);
+		return c.text(USER_MESSAGE.USER_CREATED);
+	}),
+	postUsers: tbValidator("json", T.Array(UserSchema), (result, c) => {
+		if (!result.success) {
+			throw new HTTPException(400, { message: GLOBAL_MESSAGE.INVALID_REQUEST });
+		}
+		UserRepository.createUsers(result.data satisfies Array<User>);
+		return c.text(USER_MESSAGE.USER_CREATED);
+	}),
+};
+
+/**
+ * Routing
  */
 app
-	.get("/user/:id", (c) => {
-		const id = c.req.param("id");
-		return c.json(findUserById(parseInt(id)));
-	})
-	.get("/users", (c) => {
-		return c.json(findUsers());
-	})
-	.post(
-		"/user",
-		tbValidator("json", UserSchema, (result, c) => {
-			if (!result.success) {
-				throw new HTTPException(400, { message: GLOBAL_MESSAGE.INVALID_REQUEST });
-			}
-			createUser(result.data satisfies User);
-			return c.text(USER_MESSAGE.USER_CREATED);
-		}),
-	);
+	.get("/user/:id", UserController.getUserById)
+	.get("/users", UserController.getUsers)
+	.post("/user", UserController.postUser)
+	.post("/users", UserController.postUsers);
--- app/src/api/users/repository.ts
+++ app/src/api/users/repository.ts
@@ -4,27 +4,38 @@ import { $users } from "../../db/schema";
 import { User } from "../../db/types";
 
 /**
- * ユーザーをIDで検索する
- * @param id
- * @returns
+ * ユーザーリポジトリ
  */
-export const findUserById = async (id: number): Promise<User | undefined> => {
-	return await db.select().from($users).where(eq($users.id, id)).limit(1).get();
-};
-
-/**
- * ユーザーを全件取得する
- * @returns
- */
-export const findUsers = async (): Promise<Array<User>> => {
-	return await db.select().from($users);
-};
-
-/**
- * ユーザーを作成する
- * @param user
- * @returns
- */
-export const createUser = async (user: User): Promise<void> => {
-	return await db.insert($users).values(user);
+export const UserRepository = {
+	/**
+	 * ユーザーをIDで検索する
+	 * @param id
+	 * @returns
+	 */
+	findUserById: async (id: number): Promise<User | undefined> => {
+		return await db.select().from($users).where(eq($users.id, id)).limit(1).get();
+	},
+	/**
+	 * ユーザーを全件取得する
+	 * @returns
+	 */
+	findUsers: async (): Promise<Array<User>> => {
+		return await db.select().from($users);
+	},
+	/**
+	 * ユーザーを作成する
+	 * @param user
+	 * @returns
+	 */
+	createUser: async (user: User): Promise<void> => {
+		return await db.insert($users).values(user);
+	},
+	/**
+	 * ユーザーを複数作成する
+	 * @param users
+	 * @returns
+	 */
+	createUsers: async (users: Array<User>): Promise<void> => {
+		return await db.insert($users).values(users);
+	},
 };
