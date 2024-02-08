a47b74070451274ca1c216ca6241176f1b94bb05
eyemovic-doi <doi@eyemovic.com>
2024-02-08 23:59:05 +0900

    fix: schema

--- app/src/db/schema.ts
+++ app/src/db/schema.ts
@@ -5,7 +5,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
  * users テーブル
  */
 export const $users = sqliteTable("users", {
-	id: integer("id", { mode: "number" }).primaryKey(),
+	id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
 	firstName: text("first_name").notNull(),
 	givenName: text("given_name").notNull(),
 	createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
