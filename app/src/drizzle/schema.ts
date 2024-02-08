7c5421f629373d306b21f0d0d1f0df7fc345741d
eyemovic-doi <doi@eyemovic.com>
2024-02-08 20:52:09 +0900

    chore: add comment

--- app/src/drizzle/schema.ts
+++ app/src/drizzle/schema.ts
@@ -1,5 +1,8 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+/**
+ * users テーブル
+ */
 export const $users = sqliteTable("users", {
 	id: integer("id").primaryKey(),
 	firstName: text("first_name"),
@@ -7,4 +10,11 @@ export const $users = sqliteTable("users", {
 	createdAt: text("created_at"),
 });
 
+/**
+ * User
+ * @property id - ID
+ * @property firstName - 名
+ * @property givenName - 姓
+ * @property createdAt - 作成日時
+ */
 export type User = typeof $users.$inferSelect;
