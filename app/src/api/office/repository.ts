1e3601c9063a8b2246f4da9f4befc9d500a8adc6
eyemovic-doi <doi@eyemovic.com>
2024-02-09 15:05:53 +0900

    fix: comments

--- app/src/api/office/repository.ts
+++ app/src/api/office/repository.ts
@@ -10,31 +10,24 @@ import { Office } from "../../db/types";
 export const OfficeRepository = {
 	/**
 	 * オフィスをIDで検索する
-	 * @param id
-	 * @returns
 	 */
 	findOfficeById: async (id: number): Promise<Office | undefined> => {
 		return await db.select().from($offices).where(eq($offices.id, id)).limit(1).get();
 	},
 	/**
 	 * オフィスを全件取得する
-	 * @returns
 	 */
 	findOffices: async (): Promise<Array<Office>> => {
 		return await db.select().from($offices);
 	},
 	/**
 	 * オフィスを作成する
-	 * @param office
-	 * @returns
 	 */
 	createOffice: async (office: Office): Promise<void> => {
 		return await db.insert($offices).values(office);
 	},
 	/**
 	 * オフィスを複数作成する
-	 * @param offices
-	 * @returns
 	 */
 	createOffices: async (offices: Array<Office>): Promise<void> => {
 		return await db.insert($offices).values(offices);
